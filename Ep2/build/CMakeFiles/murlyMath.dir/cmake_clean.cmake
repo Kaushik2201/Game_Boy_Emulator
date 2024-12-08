2181922dfc71ea04870e5f20b5f7fcc17c18be9d
myst <rahul.skyscc@gmail.com>
2024-12-08 23:16:51 +0530

    Final Commit with makefiles :)

---
+++ Ep2/build/CMakeFiles/murlyMath.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/murlyMath.dir/SomeLib/cpp/adder.cpp.obj"
+  "CMakeFiles/murlyMath.dir/SomeLib/cpp/adder.cpp.obj.d"
+  "libmurlyMath.a"
+  "libmurlyMath.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/murlyMath.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
