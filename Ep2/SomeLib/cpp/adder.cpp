58cc9b31001248c45d92f7039e55adef9ed63640
myst <30620767+rsmyst@users.noreply.github.com>
2024-12-15 00:06:34 +0530

    Delete Ep2 directory

--- Ep2/SomeLib/cpp/adder.cpp
+++
@@ -1,6 +0,0 @@
-#include "adder.h"
-#include <iostream>
-int murlyMath::add(int a, int b){
-        std::cout << "Sum of int A and int B is: " << a+b << std::endl;
-        return 0;
-}
--- Ep2/build/CMakeFiles/murlyMath.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for murlyMath.
-# This may be replaced when dependencies are built.
