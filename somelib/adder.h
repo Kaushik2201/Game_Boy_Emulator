464101f348e229a3f868b8c403751d45648d4753
Kishora H Shetty <kishorashetty59@gmail.com>
2024-12-26 12:13:59 +0530

    Added CPU strucure and helper macros

---
+++ somelib/adder.h
@@ -0,0 +1,6 @@
+#pragma once
+
+namespace mymath{
+    int add(int a,int b);
+    float add1(float a,float b);
+}
---
+++ testsome/main.cpp
@@ -0,0 +1,7 @@
+#include "../somelib/adder.h"
+#include<iostream>
+
+int main(){
+    std::cout<<"2+5 ="<<mymath::add(2,5)<<"\n";
+    return 0;
+}
