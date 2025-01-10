04303ef43b3053ff2e012f03dbdfb06711f433d5
Aditya Sudheer <147169353+AdityaSudheer04@users.noreply.github.com>
2025-01-10 14:11:07 +0530

    Update isa.h with some struct members, a helper function and function prototypes

--- isa.h
+++ isa.h
@@ -2,26 +2,36 @@
 #define INSTRUCTION_SET_H
 
 #include <cstdint>
-#include <functional>
-#include <unordered_map>
-#include <string>
+#include <cpu.h>
 
-// struct CPU {
-//     uint8_t A, F; 
-//     uint8_t B, C, D, E, H, L; 
-//     uint16_t SP, PC; 
 
-//     std::function<uint8_t(uint16_t)> read_memory;
-//     std::function<void(uint16_t, uint8_t)> write_memory;
-// };
-struct Instruction {
+typedef struct instruction instruction_t;
+typedef void (*instruction_func)(gbc_cpu_t *cpu, instruction_t *ins);
+
+#define INSTRUCTIONS_SET_SIZE 512
+
+#define PREFIX_CB 0xcb
+
+#define INSTRUCTION_ADD(opcode, size, func, op1, op2, c1, c2, name) {(opcode), (size), (c1), (c2), (c1), (func), ((void*)(op1)), ((void*)(op2)), 0, (name)}
+
+struct instruction {
     uint8_t opcode;
     uint8_t size;
     uint8_t cycles;
     uint8_t cycles2;
-    std::string name;
-    std::function<void()> execute;
+    uint8_t r_cycles;
+    instruction_func func;
+    void *op1;
+    void *op2;
+    union {
+        uint16_t i16;
+        uint8_t i8
+    } opcode_ext;
+    const char* name;
 };
 
+void init_instruction_set();
+instruction_t* decode(uint8_t *data);
+void int_call_i16(gbc_cpu_t *cpu, uint16_t addr);
 
 #endif 
